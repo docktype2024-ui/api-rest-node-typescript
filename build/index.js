33b6b92febb4a8ce98b51695096e77926320d136
dockie <docktype2024@gmail.com>
2025-10-03 10:27:37 +0100

    feat: add folders structure

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./server/Server");
-const PORT = process.env.PORT || 3333;
-Server_1.server.listen(PORT, () => {
-    console.log(`App rodando na porta ${PORT}`);
-});
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/' , (req,res ) => {
+    return res.send('ola ');
+});
+
 router.get('/teste' , (req,res ) => {
     return res.send('ola dev');
 });
